e3bac3807563594f0e0f243e5b848f7a5b705d1f
simone <sasso.96@libero.it>
2020-03-26 10:21:48 +0100

    fizzbuzz

---
+++ main.js
@@ -0,0 +1,21 @@
+// Fizz Buzz test:
+//  Scrivi un programma che stampi i numeri da 1 a 100,
+// ma per i multipli di 3 stampi “Fizz” al posto del numero
+// e per i multipli di 5 stampi “Buzz”.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi” FizzBuzz”.
+var i = 1;
+while (i <= 100) {
+
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log("FizzBuzz");
+  }
+  else if (i % 5 == 0) {
+    console.log("Buzz");
+  }
+  else if(i % 3 == 0){
+    console.log("Fizz");
+  }else{
+    console.log(i);
+  }
+  i++;
+}
