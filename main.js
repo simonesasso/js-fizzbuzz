18d8acba54cd0f879463ae223d409b05540148fb
simone <sasso.96@libero.it>
2020-03-26 10:43:21 +0100

    fizzbuzz-output-schermo

--- main.js
+++ main.js
@@ -3,9 +3,13 @@
 // ma per i multipli di 3 stampi “Fizz” al posto del numero
 // e per i multipli di 5 stampi “Buzz”.
 // Per i numeri che sono sia multipli di 3 che di 5 stampi” FizzBuzz”.
-var i = 1;
-while (i <= 100) {
 
+// inizializzo la var i a 1 per poi generare i numeri fino a 100 con un ciclo while
+var i = 1;
+// quando i supera 100 il ciclo termina
+while (i <= 100) {
+// le condizioni degli if sono al resto della divisione di i
+// con and le condizioni restituiscono true solo se entrambe sono rispettate
   if (i % 3 == 0 && i % 5 == 0) {
     console.log("FizzBuzz");
     var contPrec = document.getElementById('my-id').innerHTML;
@@ -13,7 +17,7 @@ while (i <= 100) {
   }
   else if (i % 5 == 0) {
     console.log("Buzz");
-    var contPrec = document.getElementById('my-id').innerHTML;
+    contPrec = document.getElementById('my-id').innerHTML;
     document.getElementById('my-id').innerHTML = contPrec + "<li>" + "Buzz" + "</li>";
   }
   else if(i % 3 == 0){
@@ -22,8 +26,10 @@ while (i <= 100) {
     document.getElementById('my-id').innerHTML = contPrec + "<li>" + "Fizz" + "</li>";
   }else{
     console.log(i);
-    var contPrec = document.getElementById('my-id').innerHTML;
+    contPrec = document.getElementById('my-id').innerHTML;
     document.getElementById('my-id').innerHTML = contPrec + "<li>" + i + "</li>";
   }
+  // incremento legato alle condizioni di uscita dal ciclo
   i++;
 }
+// per stampare a schermo salvo in contPrec il contenuto delloutput poi concateno il nuovo elemento facendolo diventare un list item
